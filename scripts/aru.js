dffac56557c01c013135a6ff2da24c84cb714ba7
esheyw <esheyw@gmail.com>
2023-12-14 12:38:25 -0800

    re-symlinked foundry, gitignore update

--- scripts/aru.js
+++ scripts/aru.js
@@ -1,5 +1,6 @@
 const fu = foundry.utils;
 const MODULE = `pf2e-actor-refresh-utility`;
+
 class ActorRefreshUtility extends FormApplication {
   static get defaultOptions() {
     return fu.mergeObject(super.defaultOptions,{
